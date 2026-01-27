957f9498323e5624e201cd61bc58bb315a8ae8da
Mohit Kumar <mohit@Mohits-MacBook-Air.local>
2026-01-27 15:15:06 +0530

    aded addtocart

--- foods/urls.py
+++ foods/urls.py
@@ -4,9 +4,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('<int:id>',views.Food_details),
-    path('allfood',views.allfood,name='allfood'),
-    path('customize',views.customize,name='customize'),
-    path('addtocart',views.addtocart,name='addtocart'),
-    path('addnewfood',views.addnewfood,name="addnewfood"),
+
+    # food list & add
+    path('allfood/', views.allfood, name='allfood'),
+    path('addnewfood/', views.addnewfood, name='addnewfood'),
+
+    # cart
+    path('addtocart/<int:id>/', views.addtocart, name='addtocart'),
+    path('cart/', views.cart, name='cart'),
+    path('cart/increase/<str:id>/', views.increase_qty, name='increase_qty'),
+    path('cart/decrease/<str:id>/', views.decrease_qty, name='decrease_qty'),
+    path('cart/remove/<str:id>/', views.remove_item, name='remove_item'),
+
+    # customize
+    path('customize/<int:id>/', views.customize, name='customize'),
+
+    # ⚠️ ALWAYS KEEP THIS LAST
+    path('<int:id>/', views.Food_details, name='food_details'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- foods/views.py
+++ foods/views.py
@@ -3,7 +3,7 @@ from foods.models import FoodItems
 from foods.forms import AddFoodForm
 
 # Create your views here.
-def Food_details(request,id=0):
+def Food_details(request,id):
     # Take the single food item details based on the id
     fooditem=FoodItems.objects.get(id=id)
     return render(request,"foods/foods_details.html",{'fooditem':fooditem})
@@ -12,11 +12,29 @@ def allfood(request):
     fooditem=FoodItems.objects.all()
     return render(request,"foods/allfood.html",{'fooditem':fooditem})
 
-def customize(request):
-    return render(request,'foods/customize.html',{})
+def customize(request, id):
+    fooditem = FoodItems.objects.get(id=id)
+    return render(request, 'foods/customize.html', {'fooditem': fooditem})
 
-def addtocart(request):
-    return render(request,'foods/addtocart.html',{})
+
+def addtocart(request, id):
+    food = FoodItems.objects.get(id=id)
+    cart = request.session.get('cart', {})
+
+    if str(id) in cart:
+        cart[str(id)]['qty'] += 1
+    else:
+        cart[str(id)] = {
+            'name': food.name,
+            'price': food.price,
+            'qty': 1,
+            'img': food.food_img.url,
+            'rating':food.rating
+        }
+
+    request.session['cart'] = cart
+    return redirect('cart')
+
 
 def addnewfood(request):
     if request.method == "POST":
@@ -28,3 +46,41 @@ def addnewfood(request):
         form = AddFoodForm()
 
     return render(request, "foods/addnewfood.html", {'form': form})
+
+def cart(request):
+    cart = request.session.get('cart', {})
+
+    total_price = 0
+    for item in cart.values():
+        total_price += item['price'] * item['qty']
+
+    context = {
+        'cart': cart,
+        'total_price': total_price
+    }
+    return render(request, 'foods/addtocart.html', context)
+
+def increase_qty(request, id):
+    cart = request.session.get('cart', {})
+    cart[id]['qty'] += 1
+    request.session['cart'] = cart
+    return redirect('cart')
+
+def decrease_qty(request, id):
+    cart = request.session.get('cart', {})
+
+    if id in cart:
+        if cart[id]['qty'] > 1:
+            cart[id]['qty'] -= 1
+        else:
+            del cart[id]   # remove item if qty becomes 0
+
+    request.session['cart'] = cart
+    return redirect('cart')
+
+
+def remove_item(request, id):
+    cart = request.session.get('cart', {})
+    del cart[id]
+    request.session['cart'] = cart
+    return redirect('cart')
